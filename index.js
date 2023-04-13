e3ce327fa9e5cd905ff70ae969fb1bf6851c696b
Juan SB <juansb.dev@gmail.com>
2023-04-13 17:43:43 -0300

    mejoras

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const options = {
   };
   
   fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', options)
-	.then(response => response.json())
+  .then(response => response.json())
 	.then(data => {
+		console.log(data)
 	  let peliApi = data.d;
-	  peliApi = peliApi.filter(elemen => elemen.i && elemen.i.imageUrl); // Filtrar elementos sin imagen
+	  peliApi = peliApi.filter(elemen => elemen.i && elemen.i.imageUrl); // Filtrar elementos sin imagen (evitamos que renderize)
 	  peliApi.map(elemen => {
 		const title = elemen.l;
 		const img = elemen.i.imageUrl;
 		const cast = elemen.s ? elemen.s : '';
 		let card = `
-		  <div class='divi'>
-			<h2> ${title} </h2>
-			<img src='${img} '> </img>
-			<p> ${cast} </p>
-		  </div>
+		<div class="card" ">
+		<img src="${img} " class="card-img-top" alt="...">
+		<div class="card-body">
+		  <h5 class="card-title">${title}</h5>
+		  <p class="card-text">${cast} </p>
+		  <a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	  </div>
 		`;
 		console.log(cast);
 		document.getElementById('container').innerHTML += card;
