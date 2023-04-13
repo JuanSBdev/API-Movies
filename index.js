2989a1a8a7d631afbcdd697615e1e99f5913dc79
Juan SB <juansb.dev@gmail.com>
2023-04-13 18:18:57 -0300

    v1.0

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+console.log('Fetching..')
+
 const options = {
 	method: 'GET',
 	headers: {
-	  'X-RapidAPI-Key': '51ba63a738mshc342e294c358014p14553bjsn60bec33c628c',
-	  'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+		'X-RapidAPI-Key': '51ba63a738mshc342e294c358014p14553bjsn60bec33c628c',
+		'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
 	}
-  };
+};
+fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', options)
+
+.then(response => response.json())
   
-  fetch('https://online-movie-database.p.rapidapi.com/auto-complete?q=spiderman', options)
-  .then(response => response.json())
 	.then(data => {
-		console.log(data)
+		console.log('Conected..')
 	  let peliApi = data.d;
 	  peliApi = peliApi.filter(elemen => elemen.i && elemen.i.imageUrl); // Filtrar elementos sin imagen (evitamos que renderize)
 	  peliApi.map(elemen => {
@@ -17,17 +20,18 @@ const options = {
 		const img = elemen.i.imageUrl;
 		const cast = elemen.s ? elemen.s : '';
 		let card = `
-		<div class="card" ">
+		<div class='card' >
 		<img src="${img} " class="card-img-top" alt="...">
 		<div class="card-body">
 		  <h5 class="card-title">${title}</h5>
 		  <p class="card-text">${cast} </p>
-		  <a href="#" class="btn btn-primary">Go somewhere</a>
+		  <a href="https://rapidapi.com/hub" class="btn btn-primary">Go somewhere</a>
 		</div>
 	  </div>
 		`;
-		console.log(cast);
 		document.getElementById('container').innerHTML += card;
+		document.getElementById('carga').style.display = 'none';
+		
 	  })
 	})
 	.catch(err => console.error(err));
